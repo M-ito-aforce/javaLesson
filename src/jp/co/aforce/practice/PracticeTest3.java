15a55a1b3f5a50a605f8b70691bd099639ddba33
M-ito-aforce <m.ito@a-force.co.jp>
2025-04-28 17:23:11 +0900

    0428

--- src/jp/co/aforce/practice/PracticeTest3.java
+++ src/jp/co/aforce/practice/PracticeTest3.java
@@ -11,31 +11,30 @@ public class PracticeTest3 {
 		itemList.add("リングノート");
 		itemList.add("クリップ");
 		itemList.add("消しゴム");
-			String element1 = itemList.get(1);
-			System.out.println(element1);
-		
-			List <String> nameList = new ArrayList<>();
-			nameList.add("山田太郎");
-			nameList.add("鈴木花子");
-			nameList.add("佐藤二郎");
-			nameList.add("山田太郎");
-			nameList.add("高橋三郎");
-				String element2 = nameList.get(2);
-				System.out.println(element2);
-		
-				ArrayList<Integer> lists = new ArrayList<>();
-					lists.add(10);
-					lists.add(11);
-					lists.add(12);
-					lists.add(15);
-					lists.add(16);
-					lists.add(18);
-					lists.add(19);
-					lists.add(20);
-					
-					for (Integer number : lists) {
-						System.out.println(number);
-					}
+		String element1 = itemList.get(1);
+		System.out.println(element1);
+		List <String> nameList = new ArrayList<>();
+		nameList.add("山田太郎");
+		nameList.add("鈴木花子");
+		nameList.add("佐藤二郎");
+		nameList.add("山田太郎");
+		nameList.add("高橋三郎");
+		String element2 = nameList.get(2);
+		System.out.println(element2);
+	
+		ArrayList<Integer> lists = new ArrayList<>();
+			lists.add(10);
+			lists.add(11);
+			lists.add(12);
+			lists.add(15);
+			lists.add(16);
+			lists.add(18);
+			lists.add(19);
+			lists.add(20);
+				
+	   for (Integer number : lists) {
+		 System.out.println(number);
+	   }
 	
 	}
 
--- src/jp/co/aforce/practice/PracticeTest5.java
+++ src/jp/co/aforce/practice/PracticeTest5.java
@@ -2,18 +2,23 @@ package jp.co.aforce.practice;
 
 public class PracticeTest5 {
 	public static void main (String[] args) {
-		int r = new java.util.Random().nextInt();
-		int num = 1;
-		switch(num) {
-		case 1:
-			System.out.println("吉です");
-		case 2:
-			System.out.println("中吉です");	
-		case 3:
-			System.out.println("大吉です");
-		default:
-			System.out.println("凶です");
+		int r = new java.util.Random().nextInt(3)+1;
+		String str = "";
+		switch(r) {
+			case 1:
+				str = "吉です";
+				break;
+			case 2:
+				str = "中吉です";
+				break;
+			case 3:
+				str = "大吉です";
+				break;
+			default:
+				str = "凶です";
+				break;
 		}
+		System.out.println(str);
 	}
-
+	
 }
